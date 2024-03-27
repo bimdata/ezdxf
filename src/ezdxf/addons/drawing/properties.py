c58e04a61750ceb73340ac1749f632a52c06f606
bdaniere <benjamin.daniere@gmail.com>
2024-03-27 10:10:16 +0100

    Fix error in drawing.property

--- src/ezdxf/addons/drawing/properties.py
+++ src/ezdxf/addons/drawing/properties.py
@@ -404,9 +404,10 @@ class RenderContext:
             self.pdsize = doc.header.get("$PDSIZE", 1.0)
             self.pdmode = doc.header.get("$PDMODE", 0)
             if doc.filename is not None:
-                self.document_dir = pathlib.Path(
-                    doc.filename.replace("\\", os.path.sep)
-                ).parent.resolve()
+                # self.document_dir = pathlib.Path(
+                #     doc.filename.replace("\\", os.path.sep)
+                # ).parent.resolve()
+                self.document_dir = pathlib.Path(doc.filename).parent.resolve()
             self._setup_text_styles(doc)
 
             try:
