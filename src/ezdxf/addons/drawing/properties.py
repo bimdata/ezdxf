c4592be0cd9aca9e68acb5debc8ed8ff4505937c
bdaniere <benjamin.daniere@gmail.com>
2024-09-12 14:43:26 +0200

    Improve get_id linetype recovery

--- src/ezdxf/addons/drawing/properties.py
+++ src/ezdxf/addons/drawing/properties.py
@@ -974,7 +974,8 @@ class RenderContext:
 
         if entity.DXFTYPE in ["LINE", "XLINE", "RAY", "POLYLINE", "LWPOLYLINE"]:
             # BIMDATA suffix for line entities
-            line_suffix = "." + entity.dxf.linetype.lower()
+            line_suffix = "." + self.resolve_linetype(entity)[0].lower()
+
         else:
             # BIMDATA suffix for hatch
             if entity.DXFTYPE == "HATCH":
